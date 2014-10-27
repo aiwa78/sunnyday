f7d052ec26880a16d83190d1b19b9aae61ec14d5
Dimitris A <aiwa78@gmail.com>
2014-10-27 08:54:21 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sunnyday::Application.config.secret_key_base = '759d8647a2e865a3a2ae42ecbb278084268d9ad98daa7ad8a1204fc1b37db8508d01dc8a5614da9bc25fa40a9974f7339c3ca4569a1a616a2bb5b2071b678b49'
